713cd5f5f33ef0c7aa5d07ee16891a4fbaef17e8
Martin Schmidt <mmartin.schmidt@gmail.com>
2021-02-13 19:43:00 -0300

    Criando repository e service para Categoria

---
+++ cursomc/src/main/java/com/martin/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.martin.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.martin.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
