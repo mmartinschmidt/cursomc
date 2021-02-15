f50cd67fce3b514c34805646b8d1a6faf617cc53
Martin Schmidt <mmartin.schmidt@gmail.com>
2021-02-14 22:30:47 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ cursomc/src/main/java/com/martin/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.martin.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.martin.cursomc.domain.Pedido;
+import com.martin.cursomc.repositories.PedidoRepository;
+import com.martin.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
